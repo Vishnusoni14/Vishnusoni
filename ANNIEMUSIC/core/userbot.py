82217e00f7e7d046d237d1461a6892712321cdec
NAINCY <vishnu14sahil25love28@gmail.com>
2024-10-29 21:47:38 +0530

    Update userbot.py

--- ANNIEMUSIC/core/userbot.py
+++ ANNIEMUSIC/core/userbot.py
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat(""https://t.me/NAINCY_UPDATES)
+                await self.one.join_chat("NAINCY_UPDATES")
               
-                await self.one.join_chat("https://t.me/+bThODpBz5adiNDJl")
+                await self.one.join_chat("+bThODpBz5adiNDJl")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("https://t.me/NAINCY_UPDATES")
+                await self.two.join_chat("NAINCY_UPDATES")
                 
-                await self.one.join_chat("https://t.me/+bThODpBz5adiNDJl")
+                await self.one.join_chat("+bThODpBz5adiNDJl")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("https://t.me/NAINCY_UPDATES")
+                await self.three.join_chat("NAINCY_UPDATES")
                 
-                await self.one.join_chat("https://t.me/+bThODpBz5adiNDJl")
+                await self.one.join_chat("+bThODpBz5adiNDJl")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("https://t.me/NAINCY_UPDATES")
+                await self.four.join_chat("NAINCY_UPDATES")
                 
-                await self.one.join_chat("https://t.me/+bThODpBz5adiNDJl")
+                await self.one.join_chat("+bThODpBz5adiNDJl")
             except:
                 pass
             assistants.append(4)
